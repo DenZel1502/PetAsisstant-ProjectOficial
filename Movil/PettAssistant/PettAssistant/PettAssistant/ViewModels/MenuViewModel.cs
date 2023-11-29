d1210d60f82e5e791ba57e63395f539cd8cf6d21
DJZuñiga <denzel.zuniga@tecsup.edu.pe>
2023-11-29 06:02:41 -0500

    commit: nuevo respositorio

---
+++ Movil/PettAssistant/PettAssistant/PettAssistant/ViewModels/MenuViewModel.cs
@@ -0,0 +1,43 @@
+﻿using PettAssistant.Views;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace PettAssistant.ViewModels
+{
+    public class MenuViewModel : BaseViewModel
+    {
+        public ICommand NavegarPMascota { get; set; }
+        public ICommand NavegarMPago { get; set; }
+        public ICommand NavegarMCuenta { get; set; }
+        public ICommand NavegarHistorial { get; set; }
+        public MenuViewModel()
+        {
+            Title = "Menú";
+            NavegarPMascota = new Command(PMascota);
+            NavegarMPago = new Command(MPago);
+            NavegarMCuenta = new Command(MCuenta);
+            NavegarHistorial = new Command(Historial);
+
+        }
+        
+        private void PMascota()
+        {
+            Application.Current.MainPage.Navigation.PushAsync(new PMascotaPage());
+        }
+        private void MPago()
+        {
+            Application.Current.MainPage.Navigation.PushAsync(new MetodoPagoPage());
+        }
+        private void MCuenta()
+        {
+            Application.Current.MainPage.Navigation.PushAsync(new DuenioPage());
+        }
+        private void Historial()
+        {
+            Application.Current.MainPage.Navigation.PushAsync(new HistorialPage());
+        }
+    }
+}
